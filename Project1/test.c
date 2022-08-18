59e84d6da8b5a819579da6c8b2b87a6a14701968
panda-player <111185068+panda-player@users.noreply.github.com>
2022-08-18 22:40:35 +0800

    Add files via upload

---
+++ Project1/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i = 1000; i < 2001; i++)
+	{
+		if (i % 4 == 0)
+		{
+			printf("%d\n", i);
+				
+		}
+	}
+
+
+
+
+	return 0;
+}
+
+
+
+		
